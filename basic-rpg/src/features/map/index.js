1e72602072c9ea5a2eb796764ba5d2a22ad8fb77
Max <mkligman1020@gmail.com>
2019-04-30 11:35:28 -0700

    beginning map tiles

--- basic-rpg/src/features/map/index.js
+++ basic-rpg/src/features/map/index.js
@@ -2,27 +2,27 @@ import React from 'react'
 
 function Map(props) {
     return(
-        // <div
-        //     style={{
-        //         position: 'relative',
-        //         top: '0px',
-        //         left: '0px',
-        //         width: '1000px',
-        //         height: '600px',
-        //         backgroundColor: 'green',
-        //         border: '4px solid black',
-        //     }}
-        // />
-        <video autoPlay loop muted id="myVideo" style={{
-            position: 'relative',
-            top: '0px',
-            left: '0px',
-            width: '1000px',
-            height: '600px',
-            border: '4px solid black',
-        }}>
-          <source src="Thick_Smoke_H264_Videvo.mp4" type="video/mp4" />
-        </video>
+        <div
+            style={{
+                position: 'relative',
+                top: '0px',
+                left: '0px',
+                width: '1000px',
+                height: '600px',
+                backgroundColor: 'green',
+                border: '4px solid black',
+            }}
+        />
+        // <video autoPlay loop muted id="myVideo" style={{
+        //     position: 'relative',
+        //     top: '0px',
+        //     left: '0px',
+        //     width: '1000px',
+        //     height: '600px',
+        //     border: '4px solid black',
+        // }}>
+        //   <source src="Thick_Smoke_H264_Videvo.mp4" type="video/mp4" />
+        // </video>
     )
 }
 
